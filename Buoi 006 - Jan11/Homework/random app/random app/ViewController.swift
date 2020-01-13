b0468b03b2d1f2464606b8575581e14daaabec8f
huyhd <huyhd.research@gmail.com>
2020-01-13 13:01:39 +0700

    update homework b6- random

--- Buoi 006 - Jan11/Homework/random app/random app/ViewController.swift
+++ Buoi 006 - Jan11/Homework/random app/random app/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
-
+/*
+ [Bài tập ôn giao diện]
+ - Tạo một UIButton và một UILabel
+ - Xử lý khi nhấn vào UIButton thì random ra một số tự nhiên và cập nhật vào UI
+ */
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var myButton: UIButton!
+    
+    var a : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onPressMyButton(_ sender: Any) {
+        self.a = Int.random(in: 0...100)
+        self.myLabel.text = "số random là \(String(self.a))"
+    }
+    
+    
 }
 
